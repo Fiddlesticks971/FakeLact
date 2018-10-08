ec9a404eabf6857fc21390261ba4c674304f10e0
fiddlesticks971 <mitchaheim@gmail.com>
2018-10-08 07:43:22 -0500

    Floating point values

--- python_client_test.py
+++ python_client_test.py
@@ -1,10 +1,18 @@
 from pymodbus3.client.sync import ModbusTcpClient
+import struct
+
+def FloatConvert(num1,num2):
+    str_output = '0x'+str(hex(num1))[2:6]+str(hex(num2))[2:6]+'000'
+    return struct.unpack('<f',struct.pack('<I', int(str_output,16)))
 
 client = ModbusTcpClient('127.0.0.1',5020)
 
-regoutput = client.read_holding_registers(10,5)
+regoutput = client.read_holding_registers(16,10)
 
-for thing in regoutput.registers:
-    print (thing)
+print (FloatConvert(regoutput.registers[0],regoutput.registers[1]))
+print (FloatConvert(regoutput.registers[2],regoutput.registers[3]))
+print (FloatConvert(regoutput.registers[4],regoutput.registers[5]))
+print (FloatConvert(regoutput.registers[6],regoutput.registers[7]))
+print (FloatConvert(regoutput.registers[8],regoutput.registers[9]))
 
 client.close()
--- python_server_test.py
+++ python_server_test.py
@@ -1,8 +1,9 @@
-from pymodbus.server.sync import StartTcpServer
+from pymodbus.server.async import StartTcpServer
 from pymodbus.device import ModbusDeviceIdentification
 from pymodbus.datastore import ModbusSequentialDataBlock
 from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
 from twisted.internet.task import LoopingCall
+import struct
 import time
 
 import logging
@@ -10,19 +11,45 @@ logging.basicConfig()
 log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
+FLOWRATE = 25.5
+INLETPRES = 50.12
+DIFFPRESS = 12.0
+OUTLETPRESS = 40.876
+BSW = 3.0
+FLOWRATE_REG = 0
+INLETPRES_REG = 2
+DIFFPRESS_REG = 4
+OUTLETPRSS_REG = 6
+BSW_REG = 8
+
+def float_to_hex(f):
+    return hex(struct.unpack('<I', struct.pack('<f', f))[0])
+
+
 def updater(a):
-    while True:
         log.debug("updating the context")
         context = a[0]
         register = 3
         slave_id = 0x00
         address = 0x10
-        log.debug("here 1")
-        values = context[slave_id].getValues(register, address, count=5)
-        values = [v + 1 for v in values]
+        values = context[slave_id].getValues(register, address, count=10)
+        flowRate = float_to_hex(FLOWRATE)
+        inletPres = float_to_hex(INLETPRES)
+        outletPress = float_to_hex(OUTLETPRESS)
+        diffPress =float_to_hex(DIFFPRESS)
+        bSW = float_to_hex(BSW)
+        values[FLOWRATE_REG] =int('0x'+flowRate[2:6],16)
+        values[FLOWRATE_REG+1] =int('0x'+flowRate[6:10],16)
+        values[INLETPRES_REG] =int('0x'+inletPres[2:6],16)
+        values[INLETPRES_REG+1] =int('0x'+inletPres[6:10],16)
+        values[DIFFPRESS_REG] =int('0x'+diffPress[2:6],16)
+        values[DIFFPRESS_REG+1] =int('0x'+diffPress[6:10],16)
+        values[OUTLETPRSS_REG] =int('0x'+outletPress[2:6],16)
+        values[OUTLETPRSS_REG+1] =int('0x'+outletPress[6:10],16)
+        values[BSW_REG] =int('0x'+bSW [2:6],16)
+        values[BSW_REG+1] =int('0x'+bSW [6:10],16)
         log.debug("new values: " + str(values))
         context[slave_id].setValues(register, address, values)
-        log.debug("here 2")
 
 store = ModbusSlaveContext(di = ModbusSequentialDataBlock(0, [17]*100),
                         co = ModbusSequentialDataBlock(0, [17]*100),
@@ -41,5 +68,5 @@ identity.MajorMinorRevision = '1.0'
 time = 5
 # 5 seconds delay
 loop = LoopingCall(f=updater, a=(context,))
-loop.start(interval=5, now=True)
+loop.start(interval=5, now=False)
 StartTcpServer(context, identity=identity, address=("localhost", 5020))
